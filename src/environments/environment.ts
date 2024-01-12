7ce73d57288d64b660bfd83b9010e78d1d1c88df
Danny Krämer <d.kraemer@dotsource.de>
2024-01-12 16:59:56 +0100

    add firebase to app

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyCXkBPgwK-w-p8r7dRRZxriG6Q4L18Hus0",
+      authDomain: "clipz-3d79d.firebaseapp.com",
+      projectId: "clipz-3d79d",
+      storageBucket: "clipz-3d79d.appspot.com",
+      messagingSenderId: "405304493958",
+      appId: "1:405304493958:web:2b6b285d91b73f92c5ce07"
+    }
+};
