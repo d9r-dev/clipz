8e21945ac86d107fe6d57542ef63cd54c7f8357e
Danny Krämer <d.kraemer@dotsource.de>
2024-01-15 17:26:04 +0100

    fix EmailTaken validation

--- src/app/user/validators/email-taken.ts
+++ src/app/user/validators/email-taken.ts
@@ -2,13 +2,15 @@ import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
 
-@Injectable()
+@Injectable({
+	providedIn: "root",
+})
 export class EmailTaken implements AsyncValidator {
 	constructor(private auth: AngularFireAuth) {}
 
-	validate(control: AbstractControl): Promise<ValidationErrors | null> {
-		this.auth
+	validate = (control: AbstractControl): Promise<ValidationErrors | null> => {
+		return this.auth
 			.fetchSignInMethodsForEmail(control.value)
 			.then((response) => (response.length ? { emailTaken: true } : null));
-	}
+	};
 }
