3c5dfe8fe6f6b7a013004a52c78087e42d83a0e5
Danny Krämer <d.kraemer@dotsource.de>
2024-01-15 17:03:48 +0100

    add authentification with firebase

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 
 @Component({
 	selector: "app-login",
@@ -11,7 +12,32 @@ export class LoginComponent {
 		password: "",
 	};
 
-	login() {
-		console.log(this.credentials);
+	showAlert = false;
+	alertMsg = "Please wait! We are logging you in.";
+	alertColor = "blue";
+	inSubmission = false;
+
+	constructor(private auth: AngularFireAuth) {}
+
+	async login() {
+		console.log("Login");
+
+		this.showAlert = true;
+		this.alertMsg = "Please wait! We are loggin you in.";
+		this.alertColor = "blue";
+		this.inSubmission = true;
+
+		try {
+			await this.auth.signInWithEmailAndPassword(this.credentials.email, this.credentials.password);
+		} catch (e) {
+			console.error(e);
+			this.inSubmission = false;
+			this.alertMsg = "An unexpected error occured. Please try again later.";
+			this.alertColor = "red";
+			return;
+		}
+
+		this.alertMsg = "Success! You are now logged in.";
+		this.alertColor = "green";
 	}
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,31 +1,62 @@
-import { Component } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {AuthService} from "../../services/auth.service";
+import IUser from "../../models/user.model";
+
 
 @Component({
-	selector: "app-register",
-	templateUrl: "./register.component.html",
-	styleUrls: ["./register.component.css"],
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-	registerForm = new FormGroup({
-		name: new FormControl("", [Validators.required, Validators.minLength(3)]),
-		email: new FormControl("", [Validators.required, Validators.email]),
-		age: new FormControl("", [Validators.required, Validators.min(18), Validators.max(120)]),
-		password: new FormControl("", [
-			Validators.required,
-			Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/gm),
-		]),
-		confirm_password: new FormControl("", [Validators.required]),
-		phoneNumber: new FormControl("", [Validators.required, Validators.minLength(13), Validators.maxLength(13)]),
-	});
+  constructor(private auth: AuthService) { }
 
-	showAlert = false;
-	alertMsg = "Please wait! Your account is being created.";
-	alertColor = "blue";
+  inSubmission = false;
 
-	register() {
-		this.showAlert = true;
-		this.alertMsg = "Please wait! Your account is being created.";
-		this.alertColor = "blue";
-	}
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl<number | null>(null, [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/gm,
+      ),
+    ]),
+    confirm_password: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(13),
+      Validators.maxLength(13),
+    ]),
+  });
+
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
+
+  async register() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.createUser(this.registerForm.value as IUser)
+    } catch (e) {
+      console.error(e);
+      this.alertMsg = 'An unexpected error occured. Please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+    this.alertMsg = 'Succes! Your account has been created.';
+    this.alertColor = 'green';
+  }
 }
